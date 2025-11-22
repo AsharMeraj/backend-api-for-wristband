f5c8f9889b0bde3e466b59c224933be1c5779938
unknown <asharmeraj55@gmail.com>
2025-11-22 17:27:26 +0500

    Fixing bug

--- index.js
+++
@@ -1,6 +0,0 @@
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-
-// Initialize Neon client inside the function
-const sql = neon(process.env.DATABASE_URL || "");
-export const db = drizzle(sql);
